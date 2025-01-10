4245829e48a5deb277028bfd7cfde5f4736f542f
Leon <83804956+Leonhoch5@users.noreply.github.com>
2025-01-10 11:28:26 +0100

    Update script.js making collision better fr

--- script.js
+++ script.js
@@ -66,10 +66,10 @@ function resolveCollisions(ball, otherBall) {
         const separationX = (overlap * dx) / distance;
         const separationY = (overlap * dy) / distance;
 
-        ball.x -= separationX / 1.1; // Move each ball half the overlap distance
-        ball.y -= separationY / 1.1;
-        otherBall.x += separationX / 1.1;
-        otherBall.y += separationY / 1.1;
+        ball.x -= separationX * this.friction; // Move each ball half the overlap distance
+        ball.y -= separationY * this.friction;
+        otherBall.x += separationX * this.friction;
+        otherBall.y += separationY * this.friction;
 
         // Calculate angle of collision
         const angle = Math.atan2(dy, dx);
